2eedfad585e2f67b166d6ebb034c1f7bfa50396a
Ankit Kumar <battu98765@gmail.com>
2026-08-03 08:37:54 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        if (n <= 2) return n; 
+
+        let prev2 = 1; 
+        let prev1 = 2; 
+
+        for (let i = 3; i <= n; i++)
+{
+    let current = prev1 + prev2 ; 
+    prev2 = prev1; 
+    prev1 = current; 
+
+}   
+
+return prev1; 
+ } 
+
+}
